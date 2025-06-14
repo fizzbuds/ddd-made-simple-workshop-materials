fe20423aacf66a08e278d0f5f2efda439b65cf9b
Gabriele Toselli <toselli.gabriele@gmail.com>
2025-06-14 17:21:52 +0200

    feat: add 1_student

---
+++ src/1_student.test.ts
@@ -0,0 +1,36 @@
+import { beforeEach, describe, expect, it } from "vitest";
+
+describe("Student", () => {
+  describe("Given a student", () => {
+    let student: Student;
+
+    beforeEach(() => {
+      student = new Student();
+    });
+
+    it("should start with a total credit amount of 0", () => {
+      expect(student.getTotalCreditAmount()).toBe(0);
+    });
+
+    describe("When adding a fee", () => {
+      beforeEach(() => {
+        student.addFee(100);
+      });
+
+      it("should increase the total credit amount", () => {
+        expect(student.getTotalCreditAmount()).toBe(100);
+      });
+    });
+
+    describe("When adding multiple fees", () => {
+      beforeEach(() => {
+        student.addFee(100);
+        student.addFee(200);
+      });
+
+      it("should calculate the correct total credit amount", () => {
+        expect(student.getTotalCreditAmount()).toBe(300);
+      });
+    });
+  });
+});
